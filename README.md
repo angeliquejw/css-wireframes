5a50d72b0a7691a4d5f5fcaceb32a06f1e309de5
Angelique Weger <hello@angeliqueweger.com>
2017-06-03 16:49:36 -0400

    Major overhaul to suit my purposes.

--- README.md
+++ README.md
@@ -1,57 +1,9 @@
-# ![CSS Wireframes Logo](images/css-wireframes-logo-32.png) css-wireframes
-
-A CSS framework to quickly design responsive wireframes directly in HTML.
+Modified version of [Les Développements Durables' css-wireframes](http://wireframes.ldd.fr/):
+* Simplified available elements (removed extra colors, ratios, optional sections and notes)
+* Converted CSS class names to use `data-wf-*` elements to make it simpler to convert this markup for production
+* Added [include-media](http://include-media.com)
+* Added [Ghost Grid](http://reklino.github.io/ghost-grid/demo/)
+* Added tooltips used in demo to main project
+* Removed tinytypo integration
 
 ![CSS Wireframes Screenshot](images/css-wireframes-screenshot.jpg)
-
-## Getting started
-
-1. Download a release, then put the CSS and Javascript files in your project folders.
-
-2. Include your favorite CSS grid system in the `<head>`, then the wireframe CSS and Javascript.
-
-``` html
-<!-- Grid system of your choice -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/bootstrap/4.0.0-alpha.6/css/bootstrap-grid.min.css" type="text/css" />
-<!-- Wireframes CSS -->
-<link rel="stylesheet" href="css/wireframes.css" type="text/css" />
-<!-- Wireframes Javascript -->
-<script type="text/javascript" src="javascript/wireframes.js"></script>
-    …
-```
-
-3. Compose your ergonomy by putting blocks where you want, and apply wireframe classes when needed.
-
-``` html
-<div class="container">
-  <div class="wf-block">Header</div>
-
-  <div class="row">
-    <div class="col">
-      <h1>
-        <span class="wf-text-lines wf-text--important">
-          Lorem ipsum dolor sit amet
-        </span>
-      </h1>
-      <p>
-        <span class="wf-text-lines">Vestibulum cras condimentum dis ullamcorper mattis dictumst interdum a commodo a parturient.</span>
-      </p>
-    </div>
-    <div class="col-md-4 wf-optional">
-      <div class="wf-block wf-block--fill">
-        Navigation
-      </div>
-    </div>
-  </div>
-</div>
-```
-
-## Documentation
-
-Check out [our documentation](http://wireframes.ldd.fr/examples/) for a live demo and detailled explanations.
-
-## Credits
-
-### Icon set
-The icons are based on the [sketchy icon set by Alice Mortaro](https://thenounproject.com/allie.fanni/collection/sketchy-wireframe/).
-Licence : [Creative Commons](http://creativecommons.org/licenses/by/3.0/us/) 
--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,53 @@
-// Include dependancies
+// Include dependencies
 var
     gulp = require('gulp'),
-    plugins = require('gulp-load-plugins')(),
-    sass = require('gulp-sass'),
-    postcss = require('gulp-postcss'),
-    sourcemaps = require('gulp-sourcemaps'),
     autoprefixer = require('autoprefixer'),
+    browserSync = require('browser-sync').create(),
+    clean = require('gulp-clean'),
     cleanCSS = require('gulp-clean-css'),
-    minify = require('gulp-minify'),
-    rename = require("gulp-rename"),
     cssBase64 = require('gulp-css-base64'),
-    clean = require('gulp-clean')
-;
+    minify = require('gulp-minify'),
+    plugins = require('gulp-load-plugins')(),
+    postcss = require('gulp-postcss'),
+    rename = require("gulp-rename"),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps');
 
 // Default task
-gulp.task('default', function() {});
+gulp.task('default', function () {});
+
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: "./dist"
+    });
+});
+
+gulp.task('reload', function (done) {
+    browserSync.reload();
+    done();
+});
 
 // Monitor changes on files and launch various tasks
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     // CSS stuff
-    gulp.watch('./src/**/*.scss', gulp.series('sass', 'autoprefixer', 'base64', 'minify-css', 'sourcemaps'));
+    gulp.watch('./src/**/*.scss', gulp.series('sass', 'autoprefixer', 'base64', 'minify-css', 'sourcemaps', 'reload'));
     // Javascript stuff
-    gulp.watch('./src/javascript/*.js', gulp.series('minify-js'));
+    gulp.watch('./src/javascript/*.js', gulp.series('minify-js', 'reload'));
     // Fontello
     gulp.watch('./src/config.json', gulp.series('fontello-import', 'fontello-deploy'));
+    // Markup
+    gulp.watch('./dist/*.html', gulp.series('reload'));
 });
 
-// Chain of tasks to build the CSS
-// FIXME : doesn't work !
-gulp.task('build-css', function(done) {
-    gulp.series('sass', 'autoprefixer', 'base64', 'minify-css', 'sourcemaps');
-    done();
-});
-
-// Chain of tasks to build the Javascript (well, only one task at the moment)
-// FIXME : doesn't work !
-gulp.task('build-js', function(done) {
-    gulp.series('minify-js');
-    done();
-});
-
-// Compile the main SASS file to CSS
-gulp.task('sass', function() {
+// Compile the main Sass file to CSS
+gulp.task('sass', function () {
     return gulp.src('./src/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
 });
 
 // Add vendor prefixes to the compiled CSS file
-gulp.task('autoprefixer', function() {
+gulp.task('autoprefixer', function () {
     return gulp.src('./dist/css/*.css')
         .pipe(postcss(
             [autoprefixer()]
@@ -57,7 +56,7 @@ gulp.task('autoprefixer', function() {
 });
 
 // Converts all data found within a stylesheet into base64-encoded data URI strings
-gulp.task('base64', function() {
+gulp.task('base64', function () {
     return gulp.src('./dist/css/wireframes.css')
         .pipe(cssBase64({
             maxWeightResource: 131072,
@@ -67,7 +66,7 @@ gulp.task('base64', function() {
 });
 
 // Minify the compiled CSS file in a new .min.css file
-gulp.task('minify-css', function() {
+gulp.task('minify-css', function () {
     return gulp.src('./dist/css/wireframes.css')
         .pipe(cleanCSS({
             compatibility: 'ie8'
@@ -79,7 +78,7 @@ gulp.task('minify-css', function() {
 });
 
 // Minify the JS file in a new .min.js file
-gulp.task('minify-js', function() {
+gulp.task('minify-js', function () {
     return gulp.src('./src/javascript/*.js')
         .pipe(minify({
             ext: {
@@ -90,7 +89,7 @@ gulp.task('minify-js', function() {
 });
 
 // Create sourcemaps for the compiled CSS
-gulp.task('sourcemaps', function() {
+gulp.task('sourcemaps', function () {
     return gulp.src('./dist/css/*.css')
         .pipe(sourcemaps.init())
         .pipe(sourcemaps.write('.'))
@@ -102,7 +101,7 @@ gulp.task('fontello-import', function () {
     return gulp.src('./src/config.json')
         .pipe(plugins.fontello({
             font: 'font', // Destination dir for Fonts and Glyphs
-            css:  'css'    // Destination dir for CSS Styles
+            css: 'css' // Destination dir for CSS Styles
         }))
         //.pipe(plugins.print())
         .pipe(gulp.dest('./src/fontello_tmp'));
@@ -120,10 +119,12 @@ gulp.task('fontello-deploy', function (done) {
     gulp.src("./src/fontello_tmp/css/fontello.css")
         .pipe(rename("_fontello.scss"))
         .pipe(gulp.dest('./src'));
-    gulp.src('./src/fontello_tmp', {read: false})
+    gulp.src('./src/fontello_tmp', {
+            read: false
+        })
         .pipe(clean());
     done();
 });
 
 // Go go go !
-gulp.task('default', gulp.series('watch'));
+gulp.task('default', gulp.parallel('browser-sync', 'watch'));
